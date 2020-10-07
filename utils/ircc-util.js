f0db44eef30ff9c58a749d0c111a4d03c7e9e2c2
jeth318 <jesper.thornberg@me.com>
2020-10-08 00:01:12 +0200

    Add utils

---
+++ utils/ircc-util.js
@@ -0,0 +1,15 @@
+const xmlStringGenerator = irccCode => {
+    return `
+        <s:Envelope 
+            xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+            s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+            <s:Body>
+                <u:X_SendIRCC xmlns:u="urn:schemas-sony-com:service:IRCC:1">
+                <IRCCCode>${irccCode}</IRCCCode>
+                </u:X_SendIRCC>
+            </s:Body>
+        </s:Envelope>
+  `
+};
+
+module.exports = { xmlStringGenerator };
