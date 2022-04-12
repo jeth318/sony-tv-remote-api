caab668d90f7b75444e5f377ec6bc0c098865063
jeth318 <jesper.thornberg@me.com>
2022-04-12 23:21:30 +0200

    Fix deploys

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,11 @@
-#!/usr/bin/env bash
-BRANCH_NAME=$1;
+#!/bin/sh
+branch=$(git branch --show-current)
 
-if [[ $BRANCH_NAME == "master" ]]; then
-	cd /home/pi/apps/sony-tv-remote/api
+if [[ $branch == "main" ]]; then
+    echo "On main branch. Will build and ship to server"
+    npm run build
+    echo "Shipping dist to server"
+    rsync -arvz -e 'ssh -p 2244' --progress $(pwd)/dist/* jeth@10.0.128.110:/home/jeth/apps/sony-tv-remote/api
 else
-	exit 0;
+    echo "Not on main branch. Skipping build and deploy."
 fi
-
-echo "Stashing lock files "
-git stash
-
-echo "Dropping stash"
-git stash drop
-
-echo "Pulling from ${BRANCH_NAME}"
-git pull origin ${BRANCH_NAME}
-echo "Pulled successfully"
-
-echo "Installing dependencies"
-npm install
-echo "Project dependencies was installed"
-
-echo "Rebooting Sony-Tv-Remote API"
-
-if [[ $BRANCH_NAME == "master" ]]; then
-	echo "pm2 restart sony-tv-remote-api"
-else
-    exit 0;
-fi
-echo "Reboot OK"
-echo "Deployment complete, and it was a success!"
-
-exit 0
